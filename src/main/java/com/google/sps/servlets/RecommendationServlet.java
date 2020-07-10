eedf4c3f1bd68346954ab7a3bdf658a1db90fdce
Caleb <caleb.li.wheeler@gmail.com>
2020-07-10 19:37:03 +0000

    Created environment for conducting facial recognition and skeleton for recommendations

---
+++ src/main/java/com/google/sps/servlets/RecommendationServlet.java
@@ -0,0 +1,79 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+
+//Have to make a UserInfo file
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+/** Servlet that returns some example content. TODO: modify this file to handle
+comments data */
+@WebServlet("/recommend")
+public class RecommendationServlet extends HttpServlet {
+  
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws IOException {
+  
+    String username;
+    String skinColor;
+    long productID;
+
+    UserService userService = UserServiceFactory.getUserService();
+      if (!userService.isUserLoggedIn()) {
+        //May want to add in an error message later like "ERROR: User is not logged in"
+        return;
+      }
+    
+    username = userService.getCurrentUser().getEmail();
+
+    Query query = new Query("UserData");
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery results = datastore.prepare(query);
+
+    //List<UserInfo> userInfo = new ArrayList<>();
+
+    for (Entity entity : results.asIterable()) {
+
+      productID = (long) entity.getProperty("date");
+      skinColor = (String) entity.getProperty("skinColor");      
+
+      if (username.equals((String) entity.getProperty("name"))) {
+        //UserInfo info = new UserInfo(skinColor, productID);    
+        //userInfo.add(info);
+      }
+    }
+
+  }
+}
