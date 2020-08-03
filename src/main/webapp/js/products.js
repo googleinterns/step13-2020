2ee0166ed73d3e2c429ad630412128dfaf1c88b2
aliciasimon <aliciasimon@google.com>
2020-08-03 15:28:31 +0000

    Updated Products JS File Aug 3

--- src/main/webapp/js/products.js
+++ src/main/webapp/js/products.js
@@ -1,6 +1,5 @@
-
 function loadUser() {
-
+ 
   fetch('/veri').then(response => response.json()).then((list) => {
     var login = document.getElementById("sign");
     var like = document.getElementById("like");
@@ -22,10 +21,10 @@ function loadUser() {
       quiz.style.visibility = "hidden";
     }
   });
-
+ 
   getProds(1);
 }
-
+ 
 function getProds(sets) {
   fetch('/product').then(response => response.json()).then((products) => {
     const productGrid = document.getElementById('prodGrid');
@@ -34,15 +33,15 @@ function getProds(sets) {
     if (sets === 1) {
       productGrid.innerHTML = "";
     }
-
+ 
     products.forEach((offer) => {
       if (count < (sets * 16) && count >= (sets * 16 - 16)) {
-
+ 
         productGrid.innerHTML += 
         "<div class=\"col-md-3 col-sm-6\">" +
             "<div class=\"product-grid\">" +
                 "<div class=\"product-image\" onclick=\"getDetails(" + offer.id + ")\">" +
-                    "<img src=\""+ offer.imgUrl +"\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
+                    "<img src=\""+ offer.imgUrl +"\" style=\"height:300px\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
                     "<button type=\"button\" class=\"quickview\">View Details</button>" +
                 "</div>" +
                 "<!-- Product Content -->" +
@@ -55,94 +54,94 @@ function getProds(sets) {
             "</div>" +
         "</div>";
       }
-
+ 
       count++;
     })
-
+ 
     document.getElementById("show").onclick = "getProds(" + (sets + 1) + ")";
   });
 }
-
+ 
 function filter(sets) {
-
+ 
   if (sets === 1) {
     document.getElementById('prodGrid').innerHTML = "";
   }
-
+ 
   var brand = "";
   var type = "";
   var tone = "";
   var vegan = "";
   var price = "";
-
+ 
   for (let i = 1; i <= 6; i++) {
     if(document.getElementById("b" + i).checked) {
       brand += document.getElementById("b" + i).value + "|";
     }
   }
-
+ 
   if (brand.length === 0) {
     brand = "None";
   }
-
+ 
   for (let i = 1; i <= 9; i++) {
     if(document.getElementById("t" + i).checked) {
       type += document.getElementById("t" + i).value + "|";
     }
   }
-
+ 
   if (type.length === 0) {
     type = "None";
   }
-
+ 
   for (let i = 1; i <= 8; i++) {
     if(document.getElementById("s" + i).checked) {
       tone += document.getElementById("s" + i).value + "|";
     }
   }
-
+ 
   if (tone.length === 0) {
     tone = "None";
   }
-
+ 
   for (let i = 1; i <= 2; i++) {
     if(document.getElementById("v" + i).checked) {
       vegan += document.getElementById("v" + i).value + "|";
     }
   }
-
+ 
   if (vegan.length === 0) {
     vegan = "None";
   }
-
+ 
   for (let i = 1; i <= 4; i++) {
     if(document.getElementById("p" + i).checked) {
       price += document.getElementById("p" + i).value + "|";
     }
   }
-
+ 
   if (price.length === 0) {
     price = "None";
   }
-
+ 
   var link = "/filter?brand=" + brand +
   "&type=" + type +
   "&tone=" + tone +
   "&vegan=" + vegan +
   "&price=" + price;
-
+ 
   fetch(link).then(response => response.json()).then((products) => {
     const productGrid = document.getElementById('prodGrid');
     var count = 0;
-
+ 
     products.forEach((offer) => {
       if (count < (sets * 16) && count >= (sets * 16 - 16)) {
-
+ 
         productGrid.innerHTML += 
         "<div class=\"col-md-3 col-sm-6\">" +
             "<div class=\"product-grid\">" +
                 "<div class=\"product-image\" onclick=\"getDetails(" + offer.id + ")\">" +
-                    "<img src=\""+ offer.imgUrl +"\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
+                    "<img src=\""+ offer.imgUrl +"\" style=\"height:300px\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
                     "<button type=\"button\" class=\"quickview\">View Details</button>" +
                 "</div>" +
                 "<!-- Product Content -->" +
@@ -155,30 +154,30 @@ function filter(sets) {
             "</div>" +
         "</div>";
       }
-
+ 
       count++;
     })
-
+ 
     document.getElementById("show").onclick = "filter(" + (sets + 1) + ")";
   });
 }
-
+ 
 function getRecs() {
-
+ 
   document.getElementById('prodGrid').innerHTML = "";
   
-
+ 
   fetch("/recommend").then(response => response.json()).then((products) => {
     const productGrid = document.getElementById('prodGrid');
     var count = 0;
-
+ 
     products.forEach((offer) => {
-
+ 
       productGrid.innerHTML += 
         "<div class=\"col-md-3 col-sm-6\">" +
             "<div class=\"product-grid\">" +
                 "<div class=\"product-image\" onclick=\"getDetails(" + offer.id + ")\">" +
-                    "<img src=\""+ offer.imgUrl +"\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
+                    "<img src=\""+ offer.imgUrl +"\" style=\"height:300px\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
                     "<button type=\"button\" class=\"quickview\">View Details</button>" +
                 "</div>" +
                 "<!-- Product Content -->" +
@@ -191,29 +190,29 @@ function getRecs() {
             "</div>" +
         "</div>";
     })
-
+ 
     document.getElementById("show").onclick = "getRecs()";
   });
 }
-
+ 
 function getLiked(sets) {
-
+ 
   if (sets === 1) {
     document.getElementById('prodGrid').innerHTML = "";
   }
-
+ 
   fetch("/getLiked").then(response => response.json()).then((products) => {
     const productGrid = document.getElementById('prodGrid');
     var count = 0;
-
+ 
     products.forEach((offer) => {
       if (count < (sets * 16) && count >= (sets * 16 - 16)) {
-
+ 
         productGrid.innerHTML += 
         "<div class=\"col-md-3 col-sm-6\">" +
             "<div class=\"product-grid\">" +
                 "<div class=\"product-image\" onclick=\"getDetails(" + offer.id + ")\">" +
-                    "<img src=\""+ offer.imgUrl +"\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
+                    "<img src=\""+ offer.imgUrl +"\" style=\"height:300px\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
                     "<button type=\"button\" class=\"quickview\">View Details</button>" +
                 "</div>" +
                 "<!-- Product Content -->" +
@@ -221,19 +220,19 @@ function getLiked(sets) {
                     "<h4 class=\"title\">" +
                     offer.name + "</h4>" +
                     "<div class=\"price\">$" + offer.cost +
-                    "<span id=\"l" + offer.id + "\" class=\"glyphicon glyphicon-heart\" onclick=\"putLiked("+ offer.id +")\"></span></div>" +
+                    "<span id=\"l" + offer.id + "\" class=\"glyphicon glyphicon-remove\" onclick=\"removeLike("+ offer.id +")\"></span></div>" +
                 "</div>" +
             "</div>" +
         "</div>";
       }
-
+ 
       count++;
     })
-
+ 
     document.getElementById("show").onclick = "getLiked(" + (sets + 1) + ")";
   });
 }
-
+ 
 function putLiked(id) {
   fetch("/liked?id=" + id).then(response => response.json()).then((result) => {
     if (result.end === "Success") {
@@ -243,30 +242,30 @@ function putLiked(id) {
     }
   });
 }
-
+ 
 function removeLike(id) {
   fetch("/delete?id=" + id).then(response => response.json()).then((result) => {
     getLiked(1);
   });
 }
-
+ 
 function search() {
   var term = document.getElementById("search").value;
-
+ 
   if (term.length === 0) {
     getProds(1);
   } else {
     fetch("/search?term=" + term).then(response => response.json()).then((products) => {
       const productGrid = document.getElementById('prodGrid');
       productGrid.innerHTML = "";
-
+ 
       products.forEach((offer) => {
         
         productGrid.innerHTML += 
         "<div class=\"col-md-3 col-sm-6\">" +
             "<div class=\"product-grid\">" +
                 "<div class=\"product-image\" onclick=\"getDetails(" + offer.id + ")\">" +
-                    "<img src=\""+ offer.imgUrl +"\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
+                    "<img src=\""+ offer.imgUrl +"\" style=\"height:300px\" onerror=\"this.onerror=null; this.src='makeup.jpg'\" width=\"300\">" +
                     "<button type=\"button\" class=\"quickview\">View Details</button>" +
                 "</div>" +
                 "<!-- Product Content -->" +
@@ -274,7 +273,7 @@ function search() {
                     "<h4 class=\"title\">" +
                     offer.name + "</h4>" +
                     "<div class=\"price\">$" + offer.cost +
-                    "<span id=\"l" + offer.id + "\" class=\"glyphicon glyphicon-heart\" onclick=\"putLiked("+ offer.id +")\"></span></div>" +
+                    "<span id=\"l" + offer.id + "\" class=\"glyphicon glyphicon-remove\" onclick=\"removeLike("+ offer.id +")\"></span></div>" +
                 "</div>" +
             "</div>" +
         "</div>";
@@ -282,39 +281,42 @@ function search() {
     });
   }
 }
-
+ 
 function getDetails(id) {
-
+ 
   fetch("/details?id=" + id).then(response => response.json()).then((products) => {
-
+ 
     document.getElementById("main_div").innerHTML = 
       "<h3>Description</h3>" +
       "<p id=\"putDefault\">" +
         "DESCRIPTION GOES HERE" +
       "</p>";
-
+ 
     if (products.description === null) {
       document.getElementById("putDefault").innerText = "No Description Available";
     } else {
       document.getElementById("putDefault").innerText = products.description;
     }
-
+ 
     document.getElementById("putDesc").innerText = products.description;
     document.getElementById("putIngs").innerText = products.ings;
     document.getElementById("brandInfo").innerText = products.brand;
     document.getElementById("nameDesc").innerText = products.name;
     document.getElementById("nameDesc").href = products.productUrl;
     document.getElementById("price").innerText = "$" + products.cost;
-    document.getElementById("product-pic-popup").onerror = "this.onerror=null; this.src='makeup.jpg'";
-
+    document.getElementById("product-pic-popup").onerror = function () {
+        this.onerror=null; 
+        this.src='makeup.jpg';
+    };
+ 
     console.log(products.imgUrl);
-
+ 
     if (products.imgUrl !== null && products.imgUrl !== "") {
       document.getElementById("product-pic-popup").src = products.imgUrl;
     } else {
       document.getElementById("product-pic-popup").src = "makeup.jpg";
     }
-
+ 
     console.log(products.rating);
     getRatings(products.rating);
     showDetails();
